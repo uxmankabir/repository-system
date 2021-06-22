8714acc60fb9140c6dfb528a4964aa60884c367e
Usman Kabir <usman.kabir.k@gmail.com>
2021-06-23 04:32:39 +0500

    repository CRUD added

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,4 +2,45 @@ class RepositoriesController < ApplicationController
   def index
     @repositories = Repository.all
   end
+
+  def new
+    @repository = Repository.new
+  end
+
+  def create
+    @repository = Repository.create(repository_params)
+    if @repository.save
+      redirect_to @repository
+    else
+      render :new
+    end
+  end
+
+  def show
+    @repository = Repository.find(params[:id])
+  end
+
+  def edit
+    @repository = Repository.find(params[:id])
+  end
+
+  def update
+    @repository = Repository.find(params[:id])
+    if @repository.update(repository_params)
+      redirect_to @repository
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    repository = Repository.find(params[:id])
+    repository.destroy
+    redirect_to repositories_path
+  end
+
+  private
+  def repository_params
+    params.require(:repository).permit(:name, :description, :user_id)
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require('jquery')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(function(){
+  $("#search_repository").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".repository-group .repository-item").filter(function() {
+      let visibility = $(this).children(".description").text().toLowerCase().indexOf(value) > -1 || $(this).children(".name").text().toLowerCase().indexOf(value) > -1
+      $(this).toggle(visibility)
+    });
+  });
+})
